6826e879c7a89d5639237ed24dcbe1d42a6833f1
Heng Sokkheng <hengsokkheng145@gmail.com>
2024-08-19 16:51:59 +0700

    top-sale

--- index.js
+++ index.js
@@ -8,6 +8,25 @@ $(document).ready(function(){
         
     });
 
+    //Top Sale
+
+    $("#top-sale .owl-carousel").owlCarousel({
+        loop:true,
+        nav:true,
+        dots:false,
+        responsive:{
+            0:{
+                items: 1
+            },
+            600:{
+                items: 3
+            },
+            1000:{
+                items: 5
+            }
+        }
+    })
+
 })
 
     
